9e7ac5a0ca2a83ba1932c35fd6f795f5041adf09
Junji Otsuka <jun531@gmail.com>
2023-03-29 10:48:05 -1000

    Third Commit

--- PlayerDuck.h
+++
@@ -1,3 +0,0 @@
-//PlayerDuck Class
-	CountdownTimer logic_timer; //Slide timer
-	int DoubleJumpCounter = 1;
--- PlayerMovement.h
+++ PlayerMovement.h
@@ -3,5 +3,9 @@ virtual void	Duck( void );
 virtual void	HandleDuckingSpeedCrop();
 virtual void	HandleSlidingSpeedCrop();
 
+//Slide timer
+CountdownTimer logic_timer; 
+
 //walking and jumping
 virtual void	FullWalkMove();
+int DoubleJumpCounter = 1;
